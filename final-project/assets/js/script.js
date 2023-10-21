3352a99945cc49c28e09399092d23fa3b018f04f
AaronCDee <aaron.chase96@gmail.com>
2023-10-21 16:54:10 +0200

    adding modules

--- final-project/assets/js/script.js
+++ final-project/assets/js/script.js
@@ -1,3 +1,4 @@
+import { generateRandomNumber } from "./assets/js/randomNumberGenerator";
 // Constants
 const quotesUri = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 const randomColors = [
@@ -54,9 +55,6 @@ const setRandomQuote = () => {
     authorText.innerText = randomQuote.author;
 }
 
-const generateRandomNumber = (max) => {
-    return Math.floor(Math.random() * max);
-}
 
 const fetchQuoteData = async () => {
     let response = await fetch(quotesUri).catch(err => alert(err));
